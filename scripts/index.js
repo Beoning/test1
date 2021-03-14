647106bda98dc932aa1c600eb08dfced69c83f0c
Даниил Никитин <faverus061999@gmail.com>
2021-03-14 20:16:20 +0300

    logic for birth

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,13 @@ const pageOne = document.getElementById("page1");
 const dateBtns = document.getElementsByClassName("btn-date");
 const btns = document.getElementsByClassName("btn1");
 const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementsByClassName("btn3");
 const pageTwo = document.getElementById("page2");
+const pageThree = document.getElementById("page3");
+const young = document.getElementById("young");
+const adult = document.getElementById("adult");
+const old = document.getElementById("old");
+const pageFour = document.getElementById("page4");
 
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", (e) => {
@@ -17,11 +23,15 @@ let month = form.month;
 let year = form.year;
 
 let dateArr = [day, month, year];
+let remember;
 
 btn2.addEventListener("click", (e) => {
   e.preventDefault;
   if (day.value != 0 && month.value != 0 && year.value != 0) {
     pageTwo.classList.add("disabled");
+    pageThree.classList.remove("disabled");
+    remember = year.value;
+    console.log(remember);
   } else {
     dateArr.forEach((elem) => {
       if (elem.value == 0) {
@@ -32,3 +42,18 @@ btn2.addEventListener("click", (e) => {
     });
   }
 });
+
+for (let i = 0; i < btn3.length; i++) {
+  btn3[i].addEventListener("click", (e) => {
+    e.stopPropagation;
+    if (remember >= 1986 && remember <= 2003) {
+      young.classList.remove("disabled");
+    } else if (remember >= 1976 && remember <= 1985) {
+      adult.classList.remove("disabled");
+    } else if (remember < 1976) {
+      old.classList.remove("disabled");
+    }
+    pageThree.classList.add("disabled");
+    pageFour.classList.remove("disabled");
+  });
+}
